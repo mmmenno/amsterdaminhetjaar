b5d3dbbae0e9a1e5c794a8e73ec0c59d5db0ae55
mmmenno <menno@denengelse.nl>
2019-03-13 11:39:00 +0100

    try curl

--- data/geojson/streetsperyear.php
+++ data/geojson/streetsperyear.php
@@ -2,15 +2,23 @@
 
 $url = "https://adamlink.nl/data/geojson/streetsperyear/" . $_GET['year'];
 
-$context = stream_context_create(array('http' => array('header'=>'Connection: close\r\n')));
-$geojson = file_get_contents($url,false,$context);
+//$context = stream_context_create(array('http' => array('header'=>'Connection: close\r\n')));
+//$geojson = file_get_contents($url,false,$context);
 
 //$geojson = file_get_contents($url);
 
-echo $geojson;
+header('Content-Type: application/json');
 
-die;
+/* Configure Curl */
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
+/* Get Response */
+$geojson = curl_exec($ch);
+
+curl_close($ch);
 
 
 
