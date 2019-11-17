1835aafb109250f49dca72e800edde239121cc4a
mmmenno <menno@denengelse.nl>
2019-11-17 12:11:09 +0100

    only exhibitions within Amsterdam

--- tentoonstellingen/tentoonstellingen.php
+++ tentoonstellingen/tentoonstellingen.php
@@ -2,11 +2,13 @@
 
 
 $sparqlQueryString = "
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
 PREFIX dc: <http://purl.org/dc/elements/1.1/>
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX wd: <http://www.wikidata.org/entity/>
 PREFIX sem: <http://semanticweb.cs.vu.nl/2009/11/sem/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
 SELECT DISTINCT ?exh ?label ?begin ?end ?place ?placelabel (SAMPLE(?img) AS ?img) WHERE {
   ?exh sem:eventType wd:Q464980 .
   ?exh rdfs:label ?label .
@@ -18,6 +20,7 @@ SELECT DISTINCT ?exh ?label ?begin ?end ?place ?placelabel (SAMPLE(?img) AS ?img
   	?cho foaf:depiction ?img .
   }
   ?place rdfs:label ?placelabel .
+  ?place wdt:P131 wd:Q9899 .
   BIND (year(?begin) AS ?startyear)
   FILTER(?startyear = " . $_GET['year'] . ")
 } 
